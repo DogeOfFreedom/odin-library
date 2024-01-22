4b1fc9f02c5bc7acce18225d10a49dd41ff5b053
DogeOfFreedom <jchensi2000@gmail.com>
2024-01-23 01:28:45 +1100

    feat: add feature to add new books

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function Book(title, author, pages, read) {
-    this.author;
-    this.title;
-    this.pages;
-    this.read;
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
 }
 
 let myLibrary = [];
-let book1 = new Book(
+const book1 = new Book(
     "Lord of the onion rings",
     "Burger King",
     49,
@@ -32,6 +32,49 @@ myLibrary.push(book2);
 myLibrary.push(book3);
 myLibrary.push(book4);
 
-function addBookToLibrary() {
+function populateBookTable() {
+    const book_table = document.querySelector("table");
 
+    for(let book of myLibrary) {
+        let {title, author, pages, read} = book;
+        let book_attributes = [title, author, pages, read];
+        const new_row = createNewBookEntry(book_attributes);
+        book_table.appendChild(new_row);        
+    }
 }
+
+function createNewBookEntry(attributes) {
+    const new_row = document.createElement("tr");
+    for(let attribute of attributes) {
+        const new_cell = document.createElement("td")
+        new_cell.textContent = attribute;
+        new_row.appendChild(new_cell);
+    }
+    return new_row;
+}
+
+
+const dialog = document.querySelector("dialog");
+const add_book_btn = document.querySelector(".add-book-btn");
+const cancel_btn = document.querySelector(".cancel-btn");
+add_book_btn.addEventListener("click", () => {
+    dialog.showModal();
+});
+cancel_btn.addEventListener("click", () => {
+    dialog.close();
+})
+
+const submit_btn = document.querySelector(".submit-btn");
+submit_btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let title = document.querySelector("input[name='title']").value;
+    let author = document.querySelector("input[name='author']").value;
+    let pages = document.querySelector("input[name='pages']").value;
+    let read = document.querySelector("select[name='read']").value;
+    const book_table = document.querySelector("table");
+    const new_row = createNewBookEntry([title, author, pages, read]);
+    book_table.appendChild(new_row);
+    dialog.close();
+})
+
+populateBookTable();
