8aa4e123ee8cc3ef2f013e5e75be0ed00f4cf5b6
DogeOfFreedom <jchensi2000@gmail.com>
2024-01-23 03:11:23 +1100

    it's 3am and i should have slept hours ago

--- script.js
+++ script.js
@@ -5,27 +5,31 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.changeReadStatus = function() {
+    this.read = this.read == "no" ? "no" : "yes";
+}
+
 let myLibrary = [];
 const book1 = new Book(
     "Lord of the onion rings",
     "Burger King",
     49,
-    false);
+    "no");
 let book2 = new Book(
     "How to kill a mean pigeon",
     "Terrance Hill",
     143,
-    false);
+    "no");
 let book3 = new Book(
     "The young men of the backlines",
     "Beatrice Alberkurkley",
     403,
-    false);
+    "no");
 let book4 = new Book(
     "My life as an astral trash lover",
     "Stella",
     203,
-    false);
+    "no");
 
 myLibrary.push(book1);
 myLibrary.push(book2);
@@ -33,26 +37,34 @@ myLibrary.push(book3);
 myLibrary.push(book4);
 
 function populateBookTable() {
-    const book_table = document.querySelector("table");
-
     for(let book of myLibrary) {
         let {title, author, pages, read} = book;
         let book_attributes = [title, author, pages, read];
-        const new_row = createNewBookEntry(book_attributes);
-        book_table.appendChild(new_row);        
+        createNewBookEntry(book_attributes);     
     }
 }
 
 function createNewBookEntry(attributes) {
-    const new_row = document.createElement("tr");
+    const book_table = document.querySelector("table");
+    const new_book_row = document.createElement("tr");
     for(let attribute of attributes) {
         const new_cell = document.createElement("td")
         new_cell.textContent = attribute;
-        new_row.appendChild(new_cell);
+        new_book_row.appendChild(new_cell);
     }
-    return new_row;
+    const new_cell = document.createElement("td")
+    new_cell.classList.add("last-row-cell");
+    const change_read_status_btn = document.createElement("button");
+    change_read_status_btn.classList.add("book-option-btn");
+    change_read_status_btn.textContent = "Change Read Status";
+    const delete_book_btn = document.createElement("button");
+    delete_book_btn.classList.add("book-option-btn");
+    delete_book_btn.textContent = "Delete";
+    new_cell.appendChild(change_read_status_btn);
+    new_cell.appendChild(delete_book_btn);
+    new_book_row.appendChild(new_cell);
+    book_table.appendChild(new_book_row);
 }
-
 
 const dialog = document.querySelector("dialog");
 const add_book_btn = document.querySelector(".add-book-btn");
@@ -71,10 +83,11 @@ submit_btn.addEventListener("click", (e) => {
     let author = document.querySelector("input[name='author']").value;
     let pages = document.querySelector("input[name='pages']").value;
     let read = document.querySelector("select[name='read']").value;
-    const book_table = document.querySelector("table");
     const new_row = createNewBookEntry([title, author, pages, read]);
     book_table.appendChild(new_row);
     dialog.close();
 })
+
+const 
 
 populateBookTable();
