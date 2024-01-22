fe1821340bb4e164533637760e8257af1ca03da3
DogeOfFreedom <jchensi2000@gmail.com>
2024-01-23 10:57:33 +1100

    Added ability to change read status and delete books

--- script.js
+++ script.js
@@ -44,28 +44,47 @@ function populateBookTable() {
     }
 }
 
+let row_counter = 1;
 function createNewBookEntry(attributes) {
+    let row_identifier = "row" + row_counter; 
     const book_table = document.querySelector("table");
     const new_book_row = document.createElement("tr");
-    for(let attribute of attributes) {
-        const new_cell = document.createElement("td")
-        new_cell.textContent = attribute;
+    new_book_row.id = row_identifier;
+    
+    for(let i=0; i<attributes.length-1; i++) {
+        const new_cell = document.createElement("td");
+        new_cell.textContent = attributes[i];
         new_book_row.appendChild(new_cell);
     }
-    const new_cell = document.createElement("td")
-    new_cell.classList.add("last-row-cell");
+    let read = attributes[3];
+    const read_status_cell = document.createElement("td");
     const change_read_status_btn = document.createElement("button");
     change_read_status_btn.classList.add("book-option-btn");
-    change_read_status_btn.textContent = "Change Read Status";
+    change_read_status_btn.textContent = read;
+    change_read_status_btn.onclick = () => {
+        let new_status = change_read_status_btn.textContent == "yes" ? "no" : "yes";
+        change_read_status_btn.textContent = new_status;
+    }
+
+    read_status_cell.appendChild(change_read_status_btn);
+    new_book_row.appendChild(read_status_cell);
+    
+    const new_cell = document.createElement("td")
+    new_cell.classList.add("last-row-cell");
     const delete_book_btn = document.createElement("button");
-    delete_book_btn.classList.add("book-option-btn");
+    delete_book_btn.classList.add("book-option-btn", "delete-book-btn", row_counter);
+    delete_book_btn.onclick = () => {
+        const row_to_remove = document.querySelector("#" + row_identifier);
+        book_table.removeChild(row_to_remove);
+    }
     delete_book_btn.textContent = "Delete";
     delete_book_btn.style.backgroundColor = "red";
     delete_book_btn.style.color = "white";
-    new_cell.appendChild(change_read_status_btn);
+
     new_cell.appendChild(delete_book_btn);
     new_book_row.appendChild(new_cell);
     book_table.appendChild(new_book_row);
+    row_counter++;
 }
 
 const dialog = document.querySelector("dialog");
@@ -85,10 +104,19 @@ submit_btn.addEventListener("click", (e) => {
     let author = document.querySelector("input[name='author']").value;
     let pages = document.querySelector("input[name='pages']").value;
     let read = document.querySelector("select[name='read']").value;
-    read = read ? "yes" : "no";
-    const new_row = createNewBookEntry([title, author, pages, read]);
-    book_table.appendChild(new_row);
+    createNewBookEntry([title, author, pages, read]);
     dialog.close();
+    
+    // Reset form
+    const title_field = document.querySelector("input[name='title']")
+    const author_field = document.querySelector("input[name='author']")
+    const pages_field = document.querySelector("input[name='pages']")
+    const read_status = document.querySelector("select[name='read']")
+    title_field.value= "";
+    author_field.value = "";
+    pages_field.value = "";
+    read_status.value = "yes";
+    console.log("cleaned up");
 })
 
 populateBookTable();
